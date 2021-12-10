62124e325d2ed6b16c321067cf64cb2b02b581a0
Ben Schomp <ben@benschomp.com>
2021-12-10 12:36:47 -0500

    some cleanup

--- day-09.py
+++ day-09.py
@@ -18,22 +18,17 @@ low_points = []
 
 # --- part one ###
 risk = 0
-y = 0
-while y < height:
-  x = 0
-  while x < width:
+for y in range(height):
+  for x in range(width):
     is_low_point = True
     for d in [(-1,0),(1,0),(0,-1),(0,1)]:
       if x+d[0] < 0 or x+d[0] >= width or y+d[1] < 0 or y+d[1] >= height:
         continue
       if grid[y+d[1]][x+d[0]] <= grid[y][x]:
-        is_low_point = False
+        is_low_point = False  # a neighbor is greater than this x,y ... not a low point
     if is_low_point:
-      low_points.append((x,y))
-      risk += int(grid[y][x]) + 1
-    x += 1
-  y += 1
-
+      low_points.append((x,y))      # store the low points for part two
+      risk += int(grid[y][x]) + 1   # risk is value at x,y + 1
 print( risk )
 
 # --- part two ---
@@ -45,8 +40,8 @@ def travel( x, y, seen ):
     if new_x < 0 or new_x >= width or new_y < 0 or new_y >= height:
       continue
     if seen[new_y][new_x] == 1 or grid[new_y][new_x] == '9':
-      continue
-    travel( new_x, new_y, seen )
+      continue  # base case: either seen it before or it's a high point
+    travel( new_x, new_y, seen )  # else, recurse on the new location
 
 basin_sizes = []
 for lp in low_points:
