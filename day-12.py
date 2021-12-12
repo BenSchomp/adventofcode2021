8adea25328ef5f313b5f75b527bf05a4ee5c9f7f
Ben Schomp <ben@benschomp.com>
2021-12-12 18:54:16 -0500

    Day 12, part two and cleanup

--- day-12.py
+++ day-12.py
@@ -4,40 +4,42 @@ for line in file:
   (left, right) = line.strip().split( '-' )
   if not left in caves:
     caves[left] = []
-  caves[left].append(right)
+  if right != 'start': # can never return to start
+    caves[left].append(right)
   if not right in caves:
     caves[right] = []
-  caves[right].append(left)
+  if left != 'start': # can never return to start
+    caves[right].append(left)
 file.close()
 
-print( caves )
-
-#      start
-#      /   \
-#  c--A----b--d
-#     \   /
-#      end
-
-count = 0
 def traverse( id, seen ):
+  #if id == 'start' and 'start' in seen:
+    #return
+
   if id[0].islower() and id in seen:
-    print( id, seen, '... lower twice' )
-    return
+    if seen[0] == None: # part one
+      return
+    elif seen[0] == True: # we've already seen a double-lower
+      return
+    seen[0] = True # this is the double-lower, mark it and continue on ...
 
   seen.append(id)
-  print( id, seen )
 
+  # found a path!
   if id == 'end':
-    print( ' *****', seen )
     global count
     count += 1
+    #print( ','.join(seen[1:]) )
     return
 
+  # else traverse to all neighboring caves
   for cur in caves[id]:
     traverse( cur, seen.copy() )
 
-  return
+count = 0
+traverse( 'start', [None] )
+print(count)
 
-
-traverse( 'start', [] )
-print( count )
+count = 0
+traverse( 'start', [False] )
+print(count)
