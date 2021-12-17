1358839fbaa39fef9df1bb0612991e13a75067fb
Ben Schomp <ben@benschomp.com>
2021-12-17 00:58:08 -0500

    Last cleanup? Mayyyybe.

--- day-17.py
+++ day-17.py
@@ -8,26 +8,25 @@ target_y = (-125, -71)
 
 hit_count = 0
 y_max_overall = 0
-overall_ma0 = [0, 0, 0]
 for init_dx in range(250):
   for init_dy in range(-250,250):
     x = y = 0
-    dx = init_dx
-    dy = init_dy
-    y_max = 0
-    count = 0
+    (dx, dy) = (init_dx, init_dy)
+    y_max = count = 0
     hit = False
+
     while True:
-      #print( "%d. (%d, %d) ... [%d, %d]" % (count, x, y, dx, dy) )
       if x >= target_x[0] and x <= target_x[1] and \
          y >= target_y[0] and y <= target_y[1]:
         hit = True
         hit_count += 1
         break
 
+      # next step
       x += dx
       y += dy
 
+      # adjust for drag and gravity
       if dx > 0:
         dx -= 1
       elif dx < 0:
@@ -38,13 +37,13 @@ for init_dx in range(250):
         y_max = y
 
       if x > target_x[1] or y < target_y[0]:
+        # gone too far
         break
 
       count += 1
 
     if hit and y_max > y_max_overall:
       y_max_overall = y_max
-      #print( '! new max:', overall_max )
 
 print( 'part one:', y_max_overall )
 print( 'part two:', hit_count )
